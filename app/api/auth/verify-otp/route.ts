4b72bed950edf6bab562df36b5a0f416d2967f12
harshil-2507 <harshilandhariya7727@gmail.com>
2025-02-19 22:29:28 +0530

    final push

--- app/api/auth/verify-otp/route.ts
+++ app/api/auth/verify-otp/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server';
 import connectDB from '../../../../lib/dbConnect';
 import User from '../../../../models/User';
 import jwt from 'jsonwebtoken';
+import nodemailer from 'nodemailer';
 
 export async function POST(request: Request) {
   const { email, otp, referralCode }: { email: string; otp: string; referralCode?: string } = await request.json();
@@ -24,6 +25,24 @@ export async function POST(request: Request) {
     user.otp = undefined;
     user.otpExpires = undefined;
     await user.save();
+
+    const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: process.env.EMAIL_USER!,
+            pass: process.env.EMAIL_PASS!,
+          },
+        });
+    
+        const mailOptions = {
+          from: process.env.EMAIL_USER!,
+          to: user.email,
+subject: 'Registration Successful - HelperBuddy',
+text: `Dear ${user.username},\n\nCongratulations! You have successfully verified your OTP and completed your registration with HelperBuddy.\n\nWe're excited to have you on board! You can now log in and start exploring our services.\n\nIf you did not register or need any assistance, please contact our support team immediately.\n\nBest regards,\nHelperBudyy Team\nhelperbuddy.gwoc@gmail.com`
+
+        };
+    
+        await transporter.sendMail(mailOptions);
 
     // Generate JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, { expiresIn: '1d' });
